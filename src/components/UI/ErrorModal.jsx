5c32c698902ca8af000c8915662c64efa486c86f
Helder Fonseca <helder2391@gmail.com>
2022-11-10 09:59:39 -0100

    add backdrop clasa

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -3,15 +3,19 @@ import { Button } from "./Button/Button";
 import './ErrorModal.css';
 
 export const ErrorModal = props => {
-  return <Card className="modal">
-          <header className="header">
-            <h2>{props.title}</h2>
-          </header>
-          <div className="content">
-            <p>{props.content}</p>
-          </div>
-          <footer className="actions">
-            <Button>Okay</Button>
-          </footer>
-        </Card>
+
+  return <>
+          <div className="backdrop"></div>
+          <Card className="modal">
+            <header className="header">
+              <h2>{props.title}</h2>
+            </header>
+            <div className="content">
+              <p>{props.content}</p>
+            </div>
+            <footer className="actions">
+              <Button>Okay</Button>
+            </footer>
+          </Card>
+        </>
 };
